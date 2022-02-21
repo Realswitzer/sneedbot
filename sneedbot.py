0753532d2554be39eba3d68084a94f21b3dbd1e9
Realswitzer <therealswitzerwicki@gmail.com>
2022-02-21 07:29:09 -0600

    fix + notoken

--- sneedbot.py
+++ sneedbot.py
@@ -1,22 +1,22 @@
 import discord
-from discord.ext    import commands
-from discord.ext.commands   import Bot
+from discord.ext import commands
+from discord.ext.commands import Bot
 import asyncio
- 
+
 bot = commands.Bot(command_prefix='#')
- 
+token = 'YOUR_TOKEN_HERE'
+
 @bot.event
 async def on_ready():
     print ("In the wise words of sneed, blessed is the man who seeds, who allows others to feed. And let us who follow Sneed, forgive the city slickers who fear those who possess the 4 of clubs. They are lost and instead follow the degenerate path of chuck, and his globohomo tendencies.")
- 
- 
+
 @bot.event
 async def on_message(message):
     channel = message.channel
     if 'sneed' in message.content:
-        await message.add_reaction(":sneed:912786155886477383")
+        await message.add_reaction(":sneedcube:945303603319537705")
     if 'SNEED' in message.content:
-        await message.add_reaction(":sneed:912786155886477383")
- 
- 
-bot.run("OTI0MzM5MDY0MTQ3MTYxMTk4.YcdH1g.M-mSqBSwg95cNgerLP0Zt70RK7w")
+        await message.add_reaction(":sneedcube:945303603319537705")
+
+
+bot.run(token)
