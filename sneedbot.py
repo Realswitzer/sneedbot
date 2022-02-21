bdffb2b131e262103dc6b3e14393b9ac763a796b
Realswitzer <therealswitzerwicki@gmail.com>
2022-02-21 13:17:53 -0600

    works with any caps rather than just sneed/SNEED

--- sneedbot.py
+++ sneedbot.py
@@ -3,19 +3,23 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 
-bot = commands.Bot(command_prefix='#')
+bot = commands.Bot(command_prefix='sneed')
 token = 'YOUR_TOKEN_HERE'
+
 
 @bot.event
 async def on_ready():
-    print ("In the wise words of sneed, blessed is the man who seeds, who allows others to feed. And let us who follow Sneed, forgive the city slickers who fear those who possess the 4 of clubs. They are lost and instead follow the degenerate path of chuck, and his globohomo tendencies.")
+    print("In the wise words of sneed, blessed is the man who seeds, who \
+allows others to feed. And let us who follow Sneed, forgive the city \
+slickers who fear those who possess the 4 of clubs. They are lost \
+and instead follow the degenerate path of chuck, and his globohomo \
+tendencies.")
+
 
 @bot.event
 async def on_message(message):
     channel = message.channel
-    if 'sneed' in message.content:
-        await message.add_reaction(":sneedcube:945303603319537705")
-    if 'SNEED' in message.content:
+    if 'sneed' in message.content.lower():
         await message.add_reaction(":sneedcube:945303603319537705")
 
 
